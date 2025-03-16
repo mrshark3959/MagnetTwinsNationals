e8416286922b84556bedfeabb25bf6fc39a40f1e
mrshark3959 <alfonsotheshark@gmail.com>
2025-03-15 21:18:38 -0400

    MUTE BUTTON FIXED

--- scripts/mutemusic.gd
+++ scripts/mutemusic.gd
@@ -1,9 +1,19 @@
 extends TextureButton
 
 @onready var musicmainm = AudioServer.get_bus_index("mmusic")
+
 func _ready() -> void:
 	AudioServer.set_bus_layout(load('res://defaultbus.tres'))
+
 func _on_pressed() -> void:
 	print("Mute button pressed")
 	$"../click".play()
-	AudioServer.set_bus_mute(musicmainm, not AudioServer.is_bus_mute(musicmainm))
+	
+	# Get the current volume of the bus
+	var current_volume = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("mmusic"))
+	
+	# Check if the volume is at 0 dB and toggle between muted and normal
+	if current_volume >= 0:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("mmusic"), -80)  # Mute
+	elif current_volume <= 0:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("mmusic"), 0)  # Unmute
