66356ccf3f386973a790e6479a9045e51318e630
mrshark3959 <alfonsotheshark@gmail.com>
2025-03-30 15:48:13 -0400

    arr

--- scripts/block.gd
+++ scripts/block.gd
@@ -7,5 +7,5 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	apply_central_impulse(Vector2(0.1, 0.1))
+#func _process(delta: float) -> void:
+	#apply_central_impulse(Vector2(0.1, 0.1))
