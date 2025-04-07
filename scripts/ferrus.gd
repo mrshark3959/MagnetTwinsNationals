60280878a7512a8275e4ffa0152871f4e213a460
Midnite59 <123998218+Midnite59@users.noreply.github.com>
2025-04-06 20:05:39 -0400

    commit

--- scripts/ferrus.gd
+++ scripts/ferrus.gd
@@ -1,16 +1,17 @@
 extends CharacterBody2D
 
-var accel = 80
 var PUSH_FORCE = 0
 var MIN_PUSH_FORCE = 5
 var is_color_red = true
+var input_direction
+@onready var pin_joint = $"PinJoint2D"
 
 func _ready() -> void:
 	pass
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * accel
+	input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction
 	if input_direction.x > 0:
 		get_node("Texture").flip_h = true
 	if input_direction.x < 0:
@@ -32,11 +33,35 @@ func get_input():
 	
 
 func _physics_process(_delta):
+	var skipmove = Vector2.ZERO
+	var fakearea = ShapeCast2D.new()
 	get_input()
-	move_and_collide(velocity*2*_delta)
+	if pin_joint.node_b != NodePath():
+		if get_node(pin_joint.node_b) is RigidBody2D:
+			fakearea.shape = get_node(pin_joint.node_b).get_node("CollisionShape2D").shape
+			fakearea.transform = get_node(pin_joint.node_b).get_node("CollisionShape2D").transform
+			fakearea.position = get_node(pin_joint.node_b).get_node("CollisionShape2D").position
+			fakearea.target_position = (velocity*160*_delta)
+			fakearea.add_exception(get_node(pin_joint.node_b))
+			get_node(pin_joint.node_b).add_child(fakearea)
+			fakearea.force_shapecast_update()
+			if fakearea.is_colliding():
+				#print("HEY")
+				pass
+			else:
+				move_and_collide(velocity*160*_delta)
+			get_node(pin_joint.node_b).remove_child(fakearea)
+	else:
+		move_and_collide(velocity*160*_delta)
+
+
+
+func bodycheck(body):
+	if body is TileMapLayer:
+		return true
 
 func pull_object():
-	pass
+	ShapeCast2D
 
 func blink():
 	if randf_range(0, 10) > 8:
