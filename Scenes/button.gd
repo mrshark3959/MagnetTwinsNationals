0798274197ecae1dd30447752d891a920d838f2d
mrshark3959 <alfonsotheshark@gmail.com>
2025-05-16 16:38:13 -0400

    new button skip

--- Scenes/button.gd
+++ Scenes/button.gd
@@ -3,7 +3,8 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$Sprite2D3.frame = 1
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,6 +13,7 @@ func _process(delta: float) -> void:
 
 
 func _on_pressed() -> void:
+	$Sprite2D3.frame = 0
 	$"../click".play()
 	$"../Timer1".start()
 	
