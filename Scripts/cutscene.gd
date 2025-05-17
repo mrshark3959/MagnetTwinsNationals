9ada89b9c9ee17a11efa9b588a0ea789d20276ca
mrshark3959 <alfonsotheshark@gmail.com>
2025-05-16 22:40:14 -0400

    cutscene basically done

---
+++ Scripts/cutscene.gd
@@ -0,0 +1,9 @@
+extends AnimationPlayer
+
+
+func _on_animation_finished(anim_name: StringName) -> void:
+	get_tree().change_scene_to_file("res://loadingscenes/loadingscreen_1.tscn")
+
+
+func _on_sprite_2d_4_ready() -> void:
+	play("cutscene")
